a78daac0574459372fb355c161daf89827cfc11f
Mo7ammad-safwat <MohammedGhaffar20648@azhar.edu.eg>
2023-11-27 14:38:09 -0800

    py

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+str = "Holberton School"
+print(str * 3, str[:9], sep='\n')
---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+str1 = "Welcome to"
+str2 = " Holberton School!"
+print(f"{str1} {str2}")
