9ae312745e150b89ae897ac25d15f475ad5b9cf8
Mo7ammad-safwat <MohammedGhaffar20648@azhar.edu.eg>
2023-12-06 13:28:21 -0800

    python

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    # Use a set to keep track of unique integers
     unique_set = set()
-
-    # Iterate through the elements in the list and add unique integers to the set
     for num in my_list:
         unique_set.add(num)
-
-    # Sum up the unique integers in the set
     result = sum(unique_set)
     return result
