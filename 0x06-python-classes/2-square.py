f70fd093f05d82f031038d2cdd0ee9ea579d39de
Mo7ammad-safwat <MohammedGhaffar20648@azhar.edu.eg>
2023-12-19 16:00:22 -0800

    python-classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""
+New class Square
+"""
+
+
 class Square:
+    """ Defines a Square """
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        if type(size) != int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
